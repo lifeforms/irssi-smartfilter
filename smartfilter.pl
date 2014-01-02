878175160c1eeaa1e1a57c6415aff844422bbedb
Walter Hop <walter@lifeforms.nl>
2014-01-02 14:34:07 +0100

    add support for filtering nick changes

--- smartfilter.pl
+++ smartfilter.pl
@@ -30,6 +30,13 @@ sub smartfilter_quit {
 	}
 };
 
+sub smartfilter_nick {
+	my ($server, $newnick, $nick, $address) = @_;
+	if ($lastmsg->{$nick} <= time() - Irssi::settings_get_int('smartfilter_delay')) {
+		Irssi::signal_stop();
+	}
+};
+
 sub log {
 	my ($server, $msg, $nick, $address, $target) = @_;
 	$lastmsg->{$nick} = time();
@@ -39,5 +46,6 @@ Irssi::signal_add('message public', 'log');
 Irssi::signal_add('message join', 'smartfilter_chan');
 Irssi::signal_add('message part', 'smartfilter_chan');
 Irssi::signal_add('message quit', 'smartfilter_quit');
+Irssi::signal_add('message nick', 'smartfilter_nick');
 
 Irssi::settings_add_int('smartfilter', 'smartfilter_delay', 300);
