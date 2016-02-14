6761c48b264dc233e4f3bf2cf56098dd25916f21
Frantisek Sumsal <frantisek@sumsal.cz>
2016-02-14 20:46:38 +0100

    Fix issues discussed in PR #5
    - Ignore function should now work as expected
    - Base user activity solely on their 'message activity'
        - This should solve the join/part spam
    - Initialize ignore_chans array only once on a script load
    - Add an option to ignore filtering of a QUIT signal
        - Behavior is unfortunately same as with NICK signal, so the signal
          is not filtered globally for all channels/servers

--- smartfilter.pl
+++ smartfilter.pl
@@ -18,6 +18,7 @@ $VERSION = "0.4";
 # Associative array of nick => last active unixtime
 our $lastmsg = {};
 our $garbagetime = 1;
+our @ignored_chans;
 
 # Do checks after receving a channel event.
 # - If the originating nick is not active, ignore the signal.
@@ -26,28 +27,15 @@ our $garbagetime = 1;
 #   or QUIT, a second nick change, etc.
 sub checkactive {
 	my ($nick, $altnick, $channel) = @_;
-	my $ignored_chans = Irssi::settings_get_str('smartfilter_ignored_chans');
-	my @ignored_chans_array = split /\s+/, $ignored_chans;
 
 	# Skip filtering if current channel is in 'smartfilter_ignored_chans'
-	if (defined $channel && grep(/$channel/, @ignored_chans_array)) {
+	if (defined $channel && grep {$_ eq $channel} @ignored_chans) {
 		return;
 	}
 
-	my $isfirst = !exists $lastmsg->{$nick};
-
-	if (!$isfirst && $lastmsg->{$nick} <= time() - Irssi::settings_get_int('smartfilter_delay')) {
+	if (!exists $lastmsg->{$nick} || $lastmsg->{$nick} <= time() - Irssi::settings_get_int('smartfilter_delay')) {
 		delete $lastmsg->{$nick};
 		Irssi::signal_stop();
-	} else {
-		$lastmsg->{$nick} = time();
-		if ($altnick) {
-			$lastmsg->{$altnick} = time();
-		}
-
-		if ($isfirst) {
-			Irssi::signal_stop();
-		}
 	}
 
 	# Run the garbage collection every interval.
@@ -75,7 +63,10 @@ sub smartfilter_chan {
 # QUIT received.
 sub smartfilter_quit {
 	my ($server, $nick, $address, $reason) = @_;
-	&checkactive($nick, undef, undef);
+
+	if (Irssi::settings_get_bool('smartfilter_filter_quit')) {
+		&checkactive($nick, undef, undef);
+	}
 };
 
 # NICK change received.
@@ -103,3 +94,7 @@ Irssi::settings_add_int('smartfilter', 'smartfilter_garbage_multiplier', 4);
 Irssi::settings_add_int('smartfilter', 'smartfilter_delay', 1200);
 Irssi::settings_add_str('smartfilter', 'smartfilter_ignored_chans', '');
 Irssi::settings_add_bool('smartfilter', 'smartfilter_filter_nick', 1);
+Irssi::settings_add_bool('smartfilter', 'smartfilter_filter_quit', 1);
+
+my $ign_chans = Irssi::settings_get_str('smartfilter_ignored_chans');
+@ignored_chans = split /\s+/, $ign_chans;
